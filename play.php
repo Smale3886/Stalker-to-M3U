2932b5ec0c167ddffb90ce3e2bf5c199dd6cd859
Smale3886 <smale3886@gmail.com>
2026-03-02 14:01:20 +0530

    Update play.php

--- play.php
+++ play.php
@@ -1,35 +1,24 @@
 <?php
 include "config.php";
+$id = $_GET['id'] ?? die("No ID");
+$token = file_exists($tokenFile) ? file_get_contents($tokenFile) : generate_token();
 
-$token = (file_exists($tokenFile)) ? file_get_contents($tokenFile) : generate_token();
-
-function fetchStreamUrl($channelId, $currentToken) {
-    global $host, $mac, $tokenFile;
-    $url = "http://$host/stalker_portal/server/load.php?type=itv&action=create_link&cmd=ffrt%20http://localhost/ch/$channelId&JsHttpRequest=1-xml";
-    $headers = [
-        "Authorization: Bearer $currentToken",
-        "Cookie: mac=$mac",
-        "User-Agent: Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG200 stbapp ver: 2 rev: 250 Safari/533.3"
-    ];
-
-    $res = Info($url, $headers);
-    $data = json_decode($res["Info_arr"]["data"], true);
-
-    if (!isset($data['js']['cmd'])) {
-        $newToken = generate_token();
-        $headers[0] = "Authorization: Bearer $newToken";
-        $res = Info($url, $headers);
-        $data = json_decode($res["Info_arr"]["data"], true);
-    }
-    return $data['js']['cmd'] ?? false;
+function getLink($id, $tk) {
+    global $host, $mac;
+    $u = "http://$host/stalker_portal/server/load.php?type=itv&action=create_link&cmd=ffrt%20http://localhost/ch/$id&JsHttpRequest=1-xml";
+    $r = Info($u, ["Authorization: Bearer $tk", "Cookie: mac=$mac"]);
+    return json_decode($r["Info_arr"]["data"], true);
 }
 
-if (!empty($_GET['id'])) {
-    $stream = fetchStreamUrl($_GET['id'], $token);
-    if ($stream) {
-        header("Location: $stream");
-        exit;
-    }
+$res = getLink($id, $token);
+if (!isset($res['js']['cmd'])) {
+    $token = generate_token();
+    $res = getLink($id, $token);
 }
-die("Error: Link generation failed.");
+
+if ($res['js']['cmd']) {
+    header("Location: " . $res['js']['cmd']);
+} else {
+    echo "Stream error.";
+}
 ?>
